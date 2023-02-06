0b23ac402eabe4966e811aedc33b53926d3f75be
Ahmed-Massoud <94191407+Ahmed-Massoud@users.noreply.github.com>
2023-02-06 12:16:22 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,85 @@
+
+
+
+
+let apps = [
+  {
+    name: "pubg mobile",
+    icon:
+      "pubg.png",
+    page: "pubg.html"
+  },
+  {
+    name: "pubg mobile",
+    icon:
+      "pubg.png",
+    page: "pubg.html"
+  },
+  {
+    name: "pubg mobile",
+    icon:
+      "pubg.png",
+    page: "pubg.html"
+  },
+  {
+    name: "pubg mobile",
+    icon:
+      "pubg.png",
+    page: "pubg.html"
+  },
+  {
+    name: "pubg mobile",
+    icon:
+      "pubg.png",
+    page: "pubg.html"
+  },
+  {
+    name: "pubg mobile",
+    icon:"pubg.png",
+    page: "pubg.html"
+  },
+  {
+    name: "pubg mobile",
+    icon:
+      "pubg.png",
+    page: "pubg.html"
+  }
+];
+Rgames = document.getElementById("Rgames");
+for (let i = 1; i <= recentNum; i++) {
+  name = apps[i - 1].name;
+  icon = apps[i - 1].icon;
+  page = apps[i - 1].page;
+  Rgames.innerHTML += `
+  <a href="games.html">
+  <div>
+                 
+                     <p>#0${i}</p>
+                 <img src=\"${icon}\" alt="">
+                  <h1>${name}</h1>
+                 </div>
+                 </a>
+                 
+  
+  `;
+}
+
+Pgames = document.getElementById("Pgames");
+for (let i = 1; i <= recentNum; i++) {
+  name = apps[i - 1].name;
+  icon = apps[i - 1].icon;
+  page = apps[i - 1].page;
+  Pgames.innerHTML += `
+    <a href="games.html">
+    <div>
+                   
+                       <p>#0${i}</p>
+                   <img src=\"${icon}\" alt="">
+                    <h1>${name}</h1>
+                   </div>
+                   </a>
+                   
+    
+    `;
+}
+
